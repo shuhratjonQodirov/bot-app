43eb408bbc2073eff945aa3f4aa6f80336fd1325
SpringDtaJpa2 <shuhratbekqodirov926@gmail.com>
2021-11-19 20:17:28 +0500

    Initial commit

---
+++ src/test/java/uz/qodirov/botapp/BotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.qodirov.botapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
